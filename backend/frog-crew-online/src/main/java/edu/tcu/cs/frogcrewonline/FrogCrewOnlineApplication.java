63ab84a56b49ef80d4bfb7e1f517eba35aa0c038
Robert-Hannaford <154273971+Robert-Hannaford@users.noreply.github.com>
2025-04-01 22:58:42 -0500

    backend project setup

---
+++ backend/frog-crew-online/src/main/java/edu/tcu/cs/frogcrewonline/FrogCrewOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcrewonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogCrewOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogCrewOnlineApplication.class, args);
+    }
+
+}
---
+++ backend/frog-crew-online/src/test/java/edu/tcu/cs/frogcrewonline/FrogCrewOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcrewonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogCrewOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
