6484124f973777ffe19df3105c72aaa2fa60a422
Robert-Hannaford <154273971+Robert-Hannaford@users.noreply.github.com>
2025-04-19 17:55:11 -0500

    Use case 16 complete

--- backend/frog-crew-online/src/main/java/edu/tcu/cs/frogcrewonline/crewmember/CrewMemberController.java
+++ backend/frog-crew-online/src/main/java/edu/tcu/cs/frogcrewonline/crewmember/CrewMemberController.java
@@ -2,11 +2,15 @@ package edu.tcu.cs.frogcrewonline.crewmember;
 
 import edu.tcu.cs.frogcrewonline.crewmember.converter.MemberDtoToMemberConverter;
 import edu.tcu.cs.frogcrewonline.crewmember.converter.MemberToMemberDtoConverter;
+import edu.tcu.cs.frogcrewonline.crewmember.converter.MemberToMemberSimpleDtoConverter;
 import edu.tcu.cs.frogcrewonline.crewmember.dto.MemberDto;
+import edu.tcu.cs.frogcrewonline.crewmember.dto.MemberSimpleDto;
 import edu.tcu.cs.frogcrewonline.system.Result;
 import edu.tcu.cs.frogcrewonline.system.StatusCode;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -17,12 +21,15 @@ public class CrewMemberController {
 
     private final MemberToMemberDtoConverter memberToMemberDtoConverter;
 
-    public CrewMemberController(CrewMemberService crewMemberService, MemberToMemberDtoConverter memberToMemberDtoConverter) {
+    private final MemberToMemberSimpleDtoConverter memberToMemberSimpleDtoConverter;
+
+    public CrewMemberController(CrewMemberService crewMemberService, MemberToMemberDtoConverter memberToMemberDtoConverter, MemberToMemberSimpleDtoConverter memberToMemberSimpleDtoConverter) {
         this.crewMemberService = crewMemberService;
         this.memberToMemberDtoConverter = memberToMemberDtoConverter;
+        this.memberToMemberSimpleDtoConverter = memberToMemberSimpleDtoConverter;
     }
 
-    //Test Case 1: add member
+    //Use Case 1: add member
     @PostMapping
     public Result addCrewMember(@Valid @RequestBody CrewMember newCrewMember) {
         CrewMember savedMember = this.crewMemberService.save(newCrewMember);
@@ -33,7 +40,7 @@ public class CrewMemberController {
 
 
 
-    //Test Case 3: find member by id
+    //Use Case 3: find member by id
     @GetMapping("/{userId}")
     public Result findCrewMemberById(@PathVariable Integer userId) {
         CrewMember  foundCrewMember = this.crewMemberService.findById(userId);
@@ -42,6 +49,21 @@ public class CrewMemberController {
     }
 
 
+    //Use Case 16: Admin views crew members
+    @GetMapping
+    public Result findAllCrewMembers() {
+        List<CrewMember> members = this.crewMemberService.findAll();
+
+        if (members.isEmpty()) {
+            return new Result(true, StatusCode.NO_CONTENT, "No Crew Members registered in the system.");
+        }
+
+        List<MemberSimpleDto> dtos = members.stream()
+                .map(this.memberToMemberSimpleDtoConverter::convert)
+                .toList();
+
+        return new Result(true, StatusCode.SUCCESS, "Find Success", dtos);
+    }
 
 
 }
